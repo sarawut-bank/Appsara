ca9c9ec195b9873c8df16db2b09d1aac92b6c407
sarawut-bank <sarwut123zz@gmail.com>
2022-07-28 23:14:16 +0700

    set size and colors text

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "My App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Flutter"),
+  runApp(MyApp());
+}
+
+//my widget
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Flutter"),
+        ),
+        body: Center(
+          child: Text(
+            "Sawaddeekub",
+            style: TextStyle(fontSize: 30, color: Colors.purple),
+          ),
+        ),
       ),
-      body: Text("Hello Bro"),
-    ),
-    theme: ThemeData(primarySwatch: Colors.yellow),
-  );
-  runApp(app);
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
+  }
 }
